6ed9c4b802687a00ede2270b85c5313a38b284c1
Lana Ferrari <ferrarirangel@gmail.com>
2021-10-26 17:55:49 +0100

    setting up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-dom";
+
 
 function App() {
+//Data
+const [events, setEvents] = useState([]);
+
+//FETCH API
+  useEffect(() => {
+    const url = `http://localhost:3030/events`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((Data) => {
+        setEvents(Data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
