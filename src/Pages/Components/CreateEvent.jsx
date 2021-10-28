0a24087435e23ef3e98d753a99956a7a1c2272f5
Lana Ferrari <ferrarirangel@gmail.com>
2021-10-28 14:43:43 +0100

    CreateEvent FormHandlers

--- src/Pages/Components/CreateEvent.jsx
+++ src/Pages/Components/CreateEvent.jsx
@@ -4,9 +4,27 @@ import { Route, Switch } from "react-router-dom";
 export default function CreateEvent(props) {
   const { setHideCreateForm, hideCreateForm } = props;
 
+  //User input
+  const [eventName, setEventName] = useState("");
+  const [eventDate, setEventDate] = useState("");
+  const [eventVenue, setEventVenue] = useState("");
+  console.log("Name,date,venue", eventName, eventDate, eventVenue);
+
   //Getting venue data
   const [venues, setVenues] = useState([]);
-  console.log("Venues", venues);
+
+  //Form Handlers
+  const handleName = (e) => setEventName(e.target.value);
+  const handleDate = (e) => setEventDate(e.target.value);
+  const handleVenue = (e) => setEventVenue(e.target.value);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newEvent = {
+      name: "name",
+    };
+  }
 
   //FETCH API
   useEffect(() => {
@@ -19,45 +37,65 @@ export default function CreateEvent(props) {
   }, []);
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <div>
-        <h2>Create Event</h2>
-      </div>
-      <div>
-        <label htmlFor="text">Name</label>
-        <input type="text" id="text" />
-      </div>
-      <div>
-        <label htmlFor="date">Date</label>
-        <input type="date" name="date" id="date" />
-      </div>
-      <div>
-        <label>Select a venue: </label>
+        <div>
+          <h2>Create Event</h2>
+        </div>
+        <div>
+          <label htmlFor="text">Name</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            onChange={handleName}
+            value={eventName}
+          />
+        </div>
+        <div>
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            name="date"
+            id="date"
+            onChange={handleDate}
+            value={eventDate}
+          />
+        </div>
+        <div>
+          <label>Select a venue: </label>
 
-        <select htmlFor="select" name="select" id="select">
-          {venues.map((venue, index) => {
-            return (
-              <option value={venue.id} id={index} key={index}>
-                {venue.buildingName}
-              </option>
-            );
-          })}
-        </select>
+          <select htmlFor="select" name="select" id="select" value={"one"}>
+            {venues.map((venue, index) => {
+              return (
+                <option
+                  value={venue.id}
+                  id={index}
+                  key={index}
+                  onChange={handleVenue}
+                  value={venue.buildingName}
+                >
+                  {venue.buildingName}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
+          <button type="button" name="button-send" id="button-send">
+            Send
+          </button>
+        </div>
+        <div>
+          <button
+            type="button-cancel"
+            id="cancel"
+            onClick={() => setHideCreateForm(!hideCreateForm)}
+          >
+            Cancel
+          </button>
+        </div>
       </div>
-      <div>
-        <button type="button" name="button-send" id="button-send">
-          Send
-        </button>
-      </div>
-      <div>
-        <button
-          type="button-cancel"
-          id="cancel"
-          onClick={() => setHideCreateForm(!hideCreateForm)}
-        >
-          Cancel
-        </button>
-      </div>
-    </div>
+    </form>
   );
 }
